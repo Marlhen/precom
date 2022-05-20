0f9f96f388e20a5ee67d1ffd2a4dcaed0423a78a
Marlhen Estrada <marlhen@gmail.com>
2022-05-20 12:17:16 -0500

    Comit inicial

---
+++ messaje.py
@@ -0,0 +1,79 @@
+### Automatizaci[o]n con Selenium TSICAM
+### By Marlhen Estrada Dic 2022 Rev 0
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from openpyxl import load_workbook
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+#### Ingresamos url, usuario y la contrasena.
+
+username = "mestradav@southernperu.com.pe"
+password = "Nuevaera202112"
+url = "https://tsicam.southernperu.com.pe/intranet/documentos/area/revision"
+
+#### Arrancamos lo automatizado
+
+driver=webdriver.Chrome(executable_path=r"C:\driver\chromedriver.exe")
+driver.get(url)
+driver.maximize_window()
+driver.find_element_by_name("username").send_keys(username)
+driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_css_selector("input.btn.btn-primary").click()
+time.sleep(1)
+
+#### Cargamos el excel donde estan los DNI[s]
+
+filesheet = "C:\\Users\\REDMIBOOK 16\\Downloads\\OneDrive_1_5-15-2022\\R19.-29-04-22\\CERTIFICADOS\\excel.xlsx"
+wb = load_workbook(filesheet)
+hojas = wb.get_sheet_names()
+print(hojas)
+nombres = wb.get_sheet_by_name('Sheet1')
+wb.close()
+
+#### Hacemos For para la cantidad de ingresos
+# dni, nomb = A y B son las columnas  y [0] empieza en la primera columna
+
+for i in range(1,20):
+    dni, nomb = nombres[f'A{i}:B{i}'][0]
+    print(dni.value, nomb.value)
+    time.sleep(3)
+    driver.find_element_by_name("nombre").send_keys(dni.value)
+    driver.find_element_by_name("nombre").send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    try:
+    ## Verifica si el selector existe.
+        item = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH,"//a[@title='Reagendar Induccion y Orientacion Básica de Seguridad - Toquepala']")))
+    #do something with the item...
+        driver.find_element_by_xpath("//a[@title='Reagendar Induccion y Orientacion Básica de Seguridad - Toquepala']").click()
+        driver.find_element_by_name("fechaSolicitud").clear()
+        time.sleep(1)
+        driver.find_element_by_name("fechaSolicitud").send_keys("11/05/2022")
+        time.sleep(1)
+        driver.find_element_by_xpath("(//a[@id='save-modal'])[1]").click()
+        time.sleep(1)
+        driver.find_element_by_css_selector(".fa.fa-check").click()
+        time.sleep(1)
+        driver.find_element_by_css_selector("#id_anexo").send_keys("C:\\Users\\REDMIBOOK 16\\Downloads\\OneDrive_1_5-15-2022\\R19.-29-04-22\\CERTIFICADOS\\"+ str(dni.value) +".pdf")
+        time.sleep(1)
+        driver.find_element_by_css_selector("#save-modal").click()
+        time.sleep(3)
+        
+        #driver.find_element_by_xpath("(//a[@class='modal-close waves-effect waves-green btn-flat'][normalize-space()='Cancelar'])[1]").click()
+        #time.sleep(1)
+        
+        driver.find_element_by_name("nombre").clear()
+        time.sleep(1)
+
+    except TimeoutException as e:
+        print("Ya esta activado")
+        pass
+        time.sleep(1)
+
+    driver.find_element_by_name("nombre").clear()
+    time.sleep(1)
---
+++ rename.py
@@ -0,0 +1,33 @@
+from itertools import count
+import pandas as pd
+import os
+
+folder = "C:\\Users\\REDMIBOOK 16\\Downloads\\OneDrive_1_5-15-2022\\R19.-29-04-22\\CERTIFICADOS\\"
+count = 1
+
+for nombrearchivo in os.listdir(folder):
+    
+    # Construct old file name
+    source = folder + nombrearchivo
+    # Adding the count to the new file name and extension
+    destination = folder + nombrearchivo.split('-')[0] + ".pdf"
+    # Renaming the file
+    #os.rename(source, destination)
+    activados = os.rename(source, destination)
+    #Creamos un diccionario
+    count += 1
+    print(str(activados) + " " + "mas mas")
+
+print('New Names are klalalalalala')
+# verify the result
+
+# Redirecciona la lista de donde tomará los datos
+res1 = os.listdir("C:\\Users\\REDMIBOOK 16\\Downloads\\OneDrive_1_5-15-2022\\R19.-29-04-22\\CERTIFICADOS\\")
+# Corta el nombre para todos los de la lista y devuelve a travez del [0] solo el nombre y de acuerdo a cada numero.
+res = [x.split('.')[0] for x in res1]
+#creando la data
+data = {'dni':res}
+df = pd.DataFrame(data)
+df.to_excel ("C:\\Users\\REDMIBOOK 16\\Downloads\\OneDrive_1_5-15-2022\\R19.-29-04-22\\CERTIFICADOS\\excel.xlsx", index = False, header=False)
+
+print(res)
